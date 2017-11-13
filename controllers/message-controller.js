cb7f8ea9b2c22b27eeb6f0504344fdbffa3644d1
Bernard Mordan <bernard@tableflip.io>
2017-11-13 11:14:55 +0000

    Update message-controller.js

--- controllers/message-controller.js
+++ controllers/message-controller.js
@@ -1,5 +1,4 @@
 module.exports = ['$scope', 'addToMessages', function ($scope, addToMessages) {
-  console.log(addToMessages.messages)
   $scope.messages = addToMessages.messages
   $scope.hideInput = addToMessages.hideInput
   $scope.hideButtons = addToMessages.hideButtons
